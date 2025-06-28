1acb251f24bed6503c18db872ead1019872a3885
Israel Adeboga <israeladeboga@Israels-MacBook-Pro-2.local>
2025-06-28 13:25:36 +0100

    Enable CORS for /simulate endpoint

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import numpy as np
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/simulate": {"origins": "*"}}, methods=["POST"])
 
 @app.route('/')
 def home():
